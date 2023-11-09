7d91a29f96fb47313a577026b020362e43419726
abhishekjadhav3470 <abhishekjadhav3470@gmail.com>
2023-11-09 10:04:04 +0530

    basic setup down

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md" , "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.3",
+    author="abhishekjadhav3470",
+    description="A small package for dvc Ml pipeline",
+    long_description=long_description,
+    long_description_content_type= "text/markdown",
+    url="https://github.com/abhishekjadhav3470/DVC-TF.git",
+    author_email="abhishekjadhav3470@gmail.com",
+    packages=["src"],
+    license="GNU",
+    python_requires =">=3.7",
+    install_requires=[
+        "dvc"
+        "tensorflow"
+        "matplotlib"
+        "numpy"
+        "pandas"
+        "tqdm"
+        "PyYAML"
+        "boto3"  
+]
+
+)
