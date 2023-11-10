b9e97e96b0dfd1e21f9a72c349101e2a83dfbf2b
abhishekjadhav3470 <abhishekjadhav3470@gmail.com>
2023-11-10 14:20:41 +0530

    callbacks

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import os
 import joblib
-import time
 import logging
 from src.utils.all_utils import get_timestamp
 
@@ -16,6 +15,7 @@ def create_and_save_tensorboard_callback(callbacks_dir, tensorboard_log_dir):
     tb_callback_filepath = os.path.join(callbacks_dir, "tensorboard_cb.cb")
     joblib.dump(tensorboard_callback, tb_callback_filepath)
     logging.info(f"tensorboard callback is being saved at {tb_callback_filepath}")
+    
 
 def create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir):
     checkpoint_file_path = os.path.join(checkpoint_dir, "ckpt_model.h5")
@@ -23,17 +23,22 @@ def create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir):
         filepath=checkpoint_file_path,
         save_best_only=True
     )
-    
+
     ckpt_callback_filepath = os.path.join(callbacks_dir, "checkpoint_cb.cb")
     joblib.dump(checkpoint_callback, ckpt_callback_filepath)
     logging.info(f"tensorboard callback is being saved at {ckpt_callback_filepath}")
 
+
 def get_callbacks(callback_dir_path):
+
     callback_path = [
         os.path.join(callback_dir_path, bin_file) for bin_file in os.listdir(callback_dir_path) if bin_file.endswith(".cb")
     ]
+
     callbacks = [
         joblib.load(path) for path in callback_path
     ]
+
     logging.info(f"saved callbacks are loaded from {callback_dir_path}")
+
     return callbacks
