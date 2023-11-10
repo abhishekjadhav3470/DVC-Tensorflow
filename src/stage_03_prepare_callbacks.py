0b44c541fabeb609e84930da3a44ca7a9e299123
abhishekjadhav3470 <abhishekjadhav3470@gmail.com>
2023-11-10 14:22:03 +0530

    bugs

--- src/stage_03_prepare_callbacks.py
+++ src/stage_03_prepare_callbacks.py
@@ -1,10 +1,8 @@
 from src.utils.all_utils import read_yaml, create_directory
-
 from src.utils.callbacks import create_and_save_tensorboard_callback, create_and_save_checkpoint_callback
 import argparse
 import os
 import logging
-import io
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
@@ -19,19 +17,18 @@ def prepare_callbacks(config_path, params_path):
     artifacts = config["artifacts"]
     artifacts_dir = artifacts["ARTIFACTS_DIR"]
 
-
-    tensorboard_log_dir = os.path.join(artifacts_dir, artifacts["TENSORBOARD_ROOT_LOG_DIR"])
+    tensorboard_log_dir = os.path.join(artifacts_dir, 
+    artifacts["TENSORBOARD_ROOT_LOG_DIR"])
 
     checkpoint_dir = os.path.join(artifacts_dir, artifacts["CHECKPOINT_DIR"])
+    
     callbacks_dir = os.path.join(artifacts_dir, artifacts["CALLBACKS_DIR"])
-
 
     create_directory([
         tensorboard_log_dir,
         checkpoint_dir,
-        callbacks_dir,
+        callbacks_dir
     ])
-
 
     create_and_save_tensorboard_callback(callbacks_dir, tensorboard_log_dir)
     create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir)
@@ -48,7 +45,7 @@ if __name__ == '__main__':
     try:
         logging.info(">>>>> stage three started")
         prepare_callbacks(config_path=parsed_args.config, params_path=parsed_args.params)
-        logging.info("stage three completed! Callbacks are prepared and saved as binary >>>>>\n")
+        logging.info("stage three completed! callbacks are prepared and saved as binary>>>>>\n")
     except Exception as e:
         logging.exception(e)
         raise e
