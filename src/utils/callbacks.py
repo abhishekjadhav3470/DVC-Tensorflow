c8cd6a71a26bb677f36067bd6cfd650d8dc1f357
abhishekjadhav3470 <abhishekjadhav3470@gmail.com>
2023-11-10 11:25:32 +0530

    callbacks

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -1,6 +1,12 @@
-def create_and_save_tensorboard_callback(callbacks_dir, tensorboard_log_dir):
-    pass
 
+
+
+
+def create_and_save_tensorboard_callback(callbacks_dir, tensorboard_log_dir):
+    unique_name = get_timestamp("tb_logs")
+
+    tb_running_log_dir = os.path.join(tensorboard_log_dir, unique_name)
+    tensorboard
 
 def  create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir):
     pass
